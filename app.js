6d9ba7a50a8354328719d7ec676a979f112cb4dd
Abdul Rehman <46538502+Abdu-Snip@users.noreply.github.com>
2021-06-25 04:26:29 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-var indexRouter = require("./routes/index");
 var productsRouter = require("./routes/products");
 var usersRouter = require("./routes/users");
 var session = require("express-session");
@@ -21,7 +20,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
 app.use("/products", productsRouter);
 app.use("/", usersRouter);
 
